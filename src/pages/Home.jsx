a43297bff3a3dcc5f43928a3e7721a622d46179b
Juan Gonon <juanfegonon27@gmail.com>
2024-06-11 17:18:21 -0600

    style home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Home</h1>
+      <Link
+        style={{
+          textDecoration: 'none',
+          background: '#444',
+          color: '#fff',
+          padding: '15px 10px',
+          borderRadius: '10px'
+        }} to='/productos'
+      >Agregar Producto
+      </Link>
+    </div>
   )
 }
